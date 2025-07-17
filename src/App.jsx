4c4ba68afedef473607d2b7d8fef9b0b2ef6d841
thbappy7706 <tanvir15-7706@diu.edu.bd>
2025-07-18 00:00:17 +0600

    1-23

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Card from "./components/Card.jsx";
 import MainLayout from "./layouts/MainLayout.jsx";
 import SearchForm from "./components/SearchForm.jsx";
 import SkeletonCard from "./components/SkeletonCard.jsx";
-import { useState } from "react";
+import {useState} from "react";
 
 function App() {
     const [search, setSearch] = useState('');
@@ -27,12 +27,12 @@ function App() {
 
     return (
         <MainLayout>
-            <SearchForm search={search} setSearch={setSearch} handleSearch={handleSearch}  loading={loading} />
+            <SearchForm search={search} setSearch={setSearch} handleSearch={handleSearch} loading={loading}/>
 
             {loading ? (
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                     {[...Array(8)].map((_, i) => (
-                        <SkeletonCard key={i} />
+                        <SkeletonCard key={i}/>
                     ))}
                 </div>
             ) : meals.length === 0 ? (
@@ -42,7 +42,7 @@ function App() {
             ) : (
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                     {meals.map((meal) => (
-                        <Card key={meal.idMeal} meal={meal} />
+                        <Card key={meal.idMeal} meal={meal}/>
                     ))}
                 </div>
             )}
