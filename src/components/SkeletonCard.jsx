a774281527e3cc5d0c212e8fdddab76635db0252
thbappy7706 <tanvir15-7706@diu.edu.bd>
2025-07-18 12:41:23 +0600

    animated duration

--- src/components/SkeletonCard.jsx
+++ src/components/SkeletonCard.jsx
@@ -1,6 +1,6 @@
 export default function SkeletonCard() {
     return (
-        <div className="animate-pulse bg-gray-800 p-4 rounded shadow-md space-y-4 dark:bg-gray-900">
+        <div style={{ animationDuration: '1s' }} className="animate-pulse !duration-500 bg-gray-800 p-4 rounded shadow-md space-y-4 dark:bg-gray-900">
             {/* Image placeholder */}
             <div className="bg-gray-700 dark:bg-gray-700 h-40 w-full rounded-md"></div>
 
